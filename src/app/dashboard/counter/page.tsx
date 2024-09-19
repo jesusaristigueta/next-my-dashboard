b80c1bdbd49f171457ef2fa3f03af27adbc45f8e
Jesus Aristigueta <jesusaristigueta@gmail.com>
2024-09-19 17:37:02 -0400

    Inicial commit

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,18 @@
+
+import { CartCounter } from '@/app/shopping-cart';
+import React from 'react';
+
+export const metadata = {
+  title: "Counter Page",
+  description: "Counter page for client side component",
+};
+
+export default function CounterPage() {
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter value={ 20 } />
+    </div>
+  );
+}
